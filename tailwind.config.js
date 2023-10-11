10120817e269819f5edf65ed6ae8b920954c43ef
Wizfie <wiz.fie@gmail.com>
2023-10-11 15:51:19 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#a8a29e",
+        dark: "#0c0a09",
+      },
+    },
+  },
+  plugins: [],
+};
